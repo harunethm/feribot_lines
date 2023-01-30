9cd774dc9957cf7f858b3c7ffe24138061e2ed8d
harunethm <harunethm@gmail.com>
2023-01-30 23:55:03 +0300

    more dummy data

--- lib/services/ferry_services.dart
+++ lib/services/ferry_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:feribot_lines/models/others/country.dart';
+import 'package:feribot_lines/models/others/key_value_model.dart';
 import 'package:feribot_lines/models/request/ferry/search_trip_request.dart';
 import 'package:feribot_lines/models/response/ferry/seaport_response.dart';
 import 'package:feribot_lines/models/response/ferry/search_trip_response.dart';
@@ -86,83 +87,94 @@ class FerryServices {
 
   static Future<List<SeaPort>> getdeparturePorts() async {
     List<SeaPort> _ports = [];
-    // await Future.delayed(const Duration(seconds: 2), () {
-    //   _ports = [
-    //     KeyValue(0, "Fethiye"),
-    //     KeyValue(1, "Atina"),
-    //     KeyValue(2, "Ayvalık"),
-    //     KeyValue(3, "Bodrum"),
-    //     KeyValue(4, "Çeşme"),
-    //     KeyValue(5, "Didim"),
-    //   ];
-    // });
+    await Future.delayed(const Duration(seconds: 2), () {
+      _ports = [
+        SeaPort(id: 0, name: "Fethiye"),
+        SeaPort(id: 1, name: "Atina"),
+        SeaPort(id: 2, name: "Ayvalık"),
+        SeaPort(id: 3, name: "Bodrum"),
+        SeaPort(id: 4, name: "Çeşme"),
+        SeaPort(id: 5, name: "Didim"),
+      ];
+    });
 
-    var url = Uri.parse(API.baseApiUrl + API.departurePorts);
-    var response = await http.get(url, headers: API.headers);
+    // var url = Uri.parse(API.baseApiUrl + API.departurePorts);
+    // var response = await http.get(url, headers: API.headers);
 
-    if (response.statusCode == 200) {
-      _ports = (jsonDecode(response.body) as List)
-          .map((e) => SeaPort.fromJson(e))
-          .toList();
-      _ports.remove(_ports.firstWhere(
-        (element) => element.id == 1638784839,
-      ));
-    } else {
-      !Get.isSnackbarOpen
-          ? Get.snackbar("Hata", "Lütfen internet bağlantınızı kontrol ediniz.")
-          : null;
-    }
+    // if (response.statusCode == 200) {
+    //   _ports = (jsonDecode(response.body) as List)
+    //       .map((e) => SeaPort.fromJson(e))
+    //       .toList();
+    //   _ports.remove(_ports.firstWhere(
+    //     (element) => element.id == 1638784839,
+    //   ));
+    // } else {
+    //   !Get.isSnackbarOpen
+    //       ? Get.snackbar("Hata", "Lütfen internet bağlantınızı kontrol ediniz.")
+    //       : null;
+    // }
     return _ports;
   }
 
   static Future<List<SeaPort>> getArrivePorts(int departurePortID) async {
     List<SeaPort> _ports = [];
-    // await Future.delayed(const Duration(seconds: 2), () {
-    //   if (departurePort == 0) {
-    //     _ports = [
-    //       KeyValue(2, "Rodos"),
-    //     ];
-    //   } else {
-    //     _ports = [
-    //       KeyValue(0, "Paros"),
-    //       KeyValue(1, "Patmos"),
-    //       KeyValue(2, "Rodos"),
-    //       KeyValue(3, "Sakarya"),
-    //       KeyValue(4, "Sarız"),
-    //       KeyValue(5, "Samoz"),
-    //     ];
-    //   }
-    // });
-    var url = Uri.parse(
-        API.baseApiUrl + API.arrivePorts + departurePortID.toString());
-    var response = await http.get(url, headers: API.headers);
+    await Future.delayed(const Duration(seconds: 2), () {
+      if (departurePortID == 0) {
+        _ports = [
+          SeaPort(id: 0, name: "Rodos"),
+        ];
+      } else {
+        _ports = [
+          SeaPort(id: 0, name: "Paros"),
+          SeaPort(id: 1, name: "Patmos"),
+          SeaPort(id: 2, name: "Rodos"),
+          SeaPort(id: 3, name: "Sakarya"),
+          SeaPort(id: 4, name: "Sarız"),
+          SeaPort(id: 5, name: "Samoz"),
+        ];
+      }
+    });
+    // var url = Uri.parse(
+    //     API.baseApiUrl + API.arrivePorts + departurePortID.toString());
+    // var response = await http.get(url, headers: API.headers);
 
-    if (response.statusCode == 200) {
-      _ports = (jsonDecode(response.body) as List)
-          .map((e) => SeaPort.fromJson(e))
-          .toList();
-    } else {
-      !Get.isSnackbarOpen
-          ? Get.snackbar("Hata", "Lütfen internet bağlantınızı kontrol ediniz.")
-          : null;
-    }
+    // if (response.statusCode == 200) {
+    //   _ports = (jsonDecode(response.body) as List)
+    //       .map((e) => SeaPort.fromJson(e))
+    //       .toList();
+    // } else {
+    //   !Get.isSnackbarOpen
+    //       ? Get.snackbar("Hata", "Lütfen internet bağlantınızı kontrol ediniz.")
+    //       : null;
+    // }
     return _ports;
   }
 
   static Future<List<Country>> getCountries() async {
     List<Country> _countries = [];
 
-    var url = Uri.parse(API.baseApiUrl + API.nationalities).toString();
-    final _dio = await _getInstance();
-    var response = await _dio.post(url);
+    await Future.delayed(const Duration(seconds: 2), () {
+      _countries = [
+        Country(id: 0, countryCode: "+1", countryName: "A Ülkesi"),
+        Country(id: 1, countryCode: "+2", countryName: "B Ülkesi"),
+        Country(id: 2, countryCode: "+3", countryName: "C Ülkesi"),
+        Country(id: 3, countryCode: "+4", countryName: "D Ülkesi"),
+        Country(id: 4, countryCode: "+5", countryName: "E Ülkesi"),
+        Country(id: 5, countryCode: "+6", countryName: "F Ülkesi"),
+      ];
+    });
 
-    if (response.statusCode == 200) {
-      _countries = response.data.map((e) => Country.fromJson(e)).toList();
-    } else {
-      !Get.isSnackbarOpen
-          ? Get.snackbar("Hata", "Lütfen internet bağlantınızı kontrol ediniz.")
-          : null;
-    }
+    // var url = Uri.parse(API.baseApiUrl + API.nationalities).toString();
+    // final _dio = await _getInstance();
+    // var response = await _dio.post(url);
+
+    // if (response.statusCode == 200) {
+    //   _countries = response.data.map((e) => Country.fromJson(e)).toList();
+    // } else {
+    //   !Get.isSnackbarOpen
+    //       ? Get.snackbar("Hata", "Lütfen internet bağlantınızı kontrol ediniz.")
+    //       : null;
+    // }
     return _countries;
   }
 }
